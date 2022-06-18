2d69ae12f3f597b9de29069a5b80a9be83346521
Efe Mesudiyeli <efemesudiyeli0@gmail.com>
2022-06-19 01:59:08 +0300

    edit, delete feat added

--- Controllers/ControlController.cs
+++ Controllers/ControlController.cs
@@ -39,6 +39,58 @@ public class ControlController : Controller
 
     }
 
+    public IActionResult DeletePatient(int id)
+    {
+
+        Console.WriteLine("delete");
+        var deletepatient = contextdata.patientInfoModels.Find(id);
+        contextdata.Remove(deletepatient);
+        contextdata.SaveChanges();
+
+
+        return RedirectToAction("Index");
+
+    }
+
+    [HttpGet]
+    public IActionResult EditPatient(int id)
+    {
+
+        Console.WriteLine("edit");
+        var editpatient = contextdata.patientInfoModels.Find(id);
+
+        return View(editpatient);
+    }
+
+    [HttpPost]
+     public IActionResult EditPatient(PatientInfoModel pm)
+    {
+
+        Console.WriteLine("edit");
+        var updatepatient = contextdata.patientInfoModels.Find(pm.id);
+        
+        updatepatient.birthDate = pm.birthDate;
+        updatepatient.birthType = pm.birthType;
+        updatepatient.bloodType = pm.bloodType;
+        updatepatient.date = pm.date;
+        updatepatient.motherBloodType = pm.motherBloodType;
+        updatepatient.fatherBloodType = pm.fatherBloodType;
+        updatepatient.patientFatherName = pm.patientFatherName;
+        updatepatient.patientMotherName = pm.patientMotherName;
+        updatepatient.patientNo = pm.patientNo;
+        updatepatient.patientHeight = pm.patientHeight;
+        updatepatient.patientWeight = pm.patientWeight;
+        updatepatient.patientSurname = pm.patientSurname;
+        updatepatient.patientName = pm.patientName;
+        updatepatient.InsertedBy = pm.InsertedBy;
+      
+
+        contextdata.SaveChanges();
+
+
+        return RedirectToAction("Index");
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
